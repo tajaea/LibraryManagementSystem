e8a6f2172c4bd95c63eb16fc7bc6e3eb01773d0c
Tajae Anglin <tajaeanglin@outlook.com>
2021-11-22 21:15:38 -0500

    made changes

--- pages/HSlibrarian.php
+++ pages/HSlibrarian.php
@@ -28,7 +28,7 @@
                 <div class="nav">
                     <div class="search">
                         <input type="text" placeholder="Search">
-                        <button type="submit"><img src="../images/search.png" alt=""></button>
+                        <button class="searchbutton" type="submit"><img src="../images/search.png" alt=""></button>
                     </div>
                     <div class="user">
                         <a href="../pages/HSlogout.php" class="btn">Log-Out</a>
@@ -42,15 +42,18 @@
                 <div class="addbook-section">
                     <div class="title">
                         <h2>Add Book</h2>
-                        <button type="submit" name="addbook-button" <?php if (isset($_POST['addbook-button'])) {
-                                                                        //include('../pages/HSaddbook.php');
-                                                                        header("location:../pages/HSaddbook.php");
-                                                                    } ?>>ADD</button>
+                        <button type="button" class="addbutton" name="addbook-button" <?php if (isset($_POST['addbook-button'])) {
+                                                                                            //include('../pages/HSaddbook.php');
+                                                                                            header("location:../pages/HSaddbook.php");
+                                                                                        } ?>><span class="addtext">Add</span>
+                            <span class="addicon">
+                                <ion-icon name="add-outline"></ion-icon>
+                            </span></button>
                     </div>
                 </div>
                 <div class="search-section">
                     <h2>Book Details</h2>
-                    <button type="submit" name="addbook-button" <?php if (isset($_POST['addbook-button'])) {
+                    <button type="submit" name="details-button" <?php if (isset($_POST['details-button'])) {
                                                                     //include('../pages/HSaddbook.php');
                                                                     header("location:../pages/HSresult.php");
                                                                 } ?>>Details</button>
@@ -58,6 +61,8 @@
             </div>
         </div>
     </form>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
 
 </html>
