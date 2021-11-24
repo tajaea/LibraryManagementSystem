9ce8cd60ad4db89fbd7963218793d731d265660b
Tajae Anglin <tajaeanglin@outlook.com>
2021-11-24 01:10:04 -0500

    removed echo for connecting to the database

--- validation/HighSchoolBooks_DB.php
+++ validation/HighSchoolBooks_DB.php
@@ -6,7 +6,7 @@ $dbas = "highschoolbooks_db";
 
 try {
     $conn = mysqli_connect($dbserver, $dbusername, $dbpassword, $dbas);
-    echo "<script>alert('You are now connected');</script>";
+    //echo "<script>alert('You are now connected');</script>";
 } catch (mysqli_sql_exception $e) {
     die('Could not connect My Sql:');
     echo "You are not connected" . $e;
