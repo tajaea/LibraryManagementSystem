5f44b0fc71bae274e9ec05de1f50d2031469f48d
Tajae Anglin <tajaeanglin@outlook.com>
2021-11-25 13:18:04 -0500

    made changes to guest

--- pages/HSguest.php
+++ pages/HSguest.php
@@ -1,14 +1,10 @@
+<?php
 
- <?php
-
-    session_start();
-    require_once '../validation/HighSchoolBooks_DB.php';
-    $query = "SELECT * FROM book";
-    $result = mysqli_query($conn, $query);
- ?>
-
-
-
+session_start();
+require_once '../validation/HighSchoolBooks_DB.php';
+$query = "SELECT * FROM book";
+$result = mysqli_query($conn, $query);
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,10 +13,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/HSguestdashboard.css" type="text/css" />
-    <script  defer type="text/javascript" src="../validation/scripts.js" ></script>
+    <script defer type="text/javascript" src="../validation/scripts.js"></script>
     <title>Guest Dashboard</title>
-
-    
 </head>
 
 <body>
@@ -32,7 +26,7 @@
             <ul>
                 <li class="dashboard-button"><img src="../images/dashboard.png" alt="">&nbsp; Dashboard</li>
                 <li><a href="HSguestsearch.php"><img src="../images/search.png" alt="">&nbsp; Search Library</a></li>
-                
+
             </ul>
             <button type="submit" class="btn" name="logout-button" <?php if (isset($_POST['logout-button'])) {
                                                                         header("location:../pages/HSlogout.php");
@@ -51,7 +45,7 @@
                         <p>Welcome, Guest</p>
                     </div>
                     <div class="user">
-                        
+
                         <div class="user-image">
                             <img src="../images/programmer.png" alt="">
                         </div>
@@ -62,39 +56,37 @@
                 <div class="available">
                     <label>Books Available</label>
                     <div class="books">
-                    <table>
-                        <?php
-                            if ($result->num_rows > 0){
-                                for($y=0;$y<$result->num_rows;$y=$y+3){
-                                    $x=0;
+                        <table>
+                            <?php
+                            if ($result->num_rows > 0) {
+                                for ($y = 0; $y < $result->num_rows; $y = $y + 3) {
+                                    $x = 0;
                                     echo "<tr>";
                                     //$row = mysqli_fetch_assoc($result);
-                                    while(($x<3)&&($row = mysqli_fetch_assoc($result))){
-                                
-                                        
-                                            echo "<td>";
-                                                //echo $row['quantity'];
-                                                echo "<img src = '../files/".$row['bookcover']."' alt = '".$row['title']."' >";
-                                                echo "<div class='overlay'>";
-                                                    echo "<div class='title' id='title'>".$row['title']."</div>";
-                                                echo "</div>";
-                                                echo "<label>Quantity:</label>".$row['quantity'];
-                                            echo "</td>";
-                                        $x=$x+1;
-                                        
+                                    while (($x < 3) && ($row = mysqli_fetch_assoc($result))) {
+
+
+                                        echo "<td>";
+                                        echo "<a href = '../pages/HSguestbookdetails.php'><img src = '../files/" . $row['bookcover'] . "' alt = '" . $row['title'] . "' ></a>";
+                                        echo "<div class='overlay'>";
+                                        echo "<div class='title' id='title'>" . $row['title'] . "</div>";
+                                        echo "</div>";
+                                        echo "<label>Quantity:</label>" . $row['quantity'];
+                                        echo "</td>";
+                                        $x = $x + 1;
                                     }
                                     echo "</tr>";
                                 }
-                            }else{
-                            echo "<script>alert('0 results')</script>";
+                            } else {
+                                echo "<script>alert('0 results')</script>";
                             }
                             mysqli_close($conn);
-                        ?>
-                        
-                    </table>
+                            ?>
+
+                        </table>
+                    </div>
                 </div>
-                </div>
-                
+
             </div>
         </div>
     </form>
---
+++ pages/HSguestbookdetails.php
@@ -0,0 +1,113 @@
+<?php
+session_start();
+require_once '../validation/HighSchoolBooks_DB.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/HSresult.css" />
+    <title>Details Dashboard</title>
+</head>
+
+<body>
+    <form action="" method="POST">
+        <div class="side-menu">
+            <div class="brand-name">
+                <h1>Guest Details</h1>
+            </div>
+            <ul>
+                <li class="dashboard-button"><img src="../images/dashboard.png" alt="">&nbsp; Dashboard</li>
+            </ul>
+            <button type="submit" class="btn" name="logout-button" <?php if (isset($_POST['logout-button'])) {
+                                                                        header("location:../pages/HSguest.php");
+                                                                    } ?>>
+                <span class="logout-text">Back</span>
+                <span class="logout-icon">
+                    <ion-icon name="arrow-back-outline"></ion-icon>
+                </span>
+            </button>
+        </div>
+        <div class="container">
+            <div class="header">
+                <img src="../images/logo.png" class="logo">
+                <div class="nav">
+                    <!--<div class="search">
+                        <input type="text" placeholder="Search">
+                        <button class="searchbutton" type="submit"><img src="../images/search.png" alt=""></button>
+                    </div>-->
+                    <div class="user">
+                        <p>Welcome, Mr. Anglin</p>
+                        <div class="user-image">
+                            <img src="../images/programmer.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="content">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Author</th>
+                            <th>Year Published</th>
+                            <th>ISBN</th>
+                            <th>Call Number</th>
+                            <th>Subject Area</th>
+                            <th>No. of Copies</th>
+                            <th>Book Cover</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        <?php
+                        $query = "SELECT * FROM book";
+                        $result = mysqli_query($conn, $query);
+                        if ($result->num_rows > 0) {
+                            while ($row = mysqli_fetch_assoc($result)) {
+
+                                echo "<tr>";
+                                echo "<td>";
+                                echo $row['title'];
+                                echo "</td>";
+                                echo "<td>";
+                                echo $row['author'];
+                                echo "</td>";
+                                echo "<td>";
+                                echo $row['year'];
+                                echo "</td>";
+                                echo "<td>";
+                                echo $row['isbn'];
+                                echo "</td>";
+                                echo "<td>";
+                                echo $row['callno'];
+                                echo "</td>";
+                                echo "<td>";
+                                echo $row['subjectarea'];
+                                echo "</td>";
+                                echo "<td>";
+                                echo $row['quantity'];
+                                echo "</td>";
+                                echo "<td>";
+                                echo "<img src = '../files/" . $row['bookcover'] . "' alt = '' >";
+                                echo "</td>";
+                                echo "</tr>";
+                            }
+                        } else {
+                            echo "<script>alert('0 results')</script>";
+                        }
+                        ?>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </form>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
