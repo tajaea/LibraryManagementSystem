24b72fdc0f65f13c9da0828a3db54ba61e8e4aa7
Tajae Anglin <tajaeanglin@outlook.com>
2021-11-26 10:59:51 -0500

    made changes

--- pages/HSadministrator.php
+++ pages/HSadministrator.php
@@ -10,7 +10,7 @@ $logged_in_user = $_GET['name'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/HSlibsettings.css" type="text/css" />
+    <link rel="stylesheet" href="../css/HSadminfunctionalities.css" type="text/css" />
     <title>Administrator Dashboard</title>
 </head>
 
